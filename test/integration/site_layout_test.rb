a1ea610751a1936199cca60746699cc7724c48c6
Moses Ogwo <mosesogwo@gmail.com>
2019-07-27 13:31:45 +0100

    update tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -18,17 +18,18 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Sign up")
 
     log_in_as(@user)
-    get root_path
-    assert_select "a[href=?]", root_path, count:2
+    assert_redirected_to @user
+    follow_redirect!
+    assert_select "h1", @user.name
+    assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", users_path
-    assert_select "a[href=?]", login_path, count:0
-    assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", "#", text: "Account"
     assert_select "a[href=?]", user_path(@user)
     assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 
 end
