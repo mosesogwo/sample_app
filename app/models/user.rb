ce1314541f6dd511a5bdb46d0fdf93218fb6e535
Moses Ogwo <mosesogwo@gmail.com>
2019-07-31 12:44:53 +0100

    add activation digest, re-seed db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
   before_save :downcase_email
   before_create :create_activation_digest
-  
+
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -43,6 +43,6 @@ class User < ApplicationRecord
 
     def create_activation_digest
       self.activation_token = User.new_token
-      self.acttivation_digest = User.digest(activation_token)
+      self.activation_digest = User.digest(activation_token)
     end
 end
