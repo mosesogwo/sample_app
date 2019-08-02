dcc21791c3da05a193eace9f772a8a8f05ffd8b5
Moses Ogwo <mosesogwo@gmail.com>
2019-08-02 10:35:33 +0100

    undo routes edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # get 'password_resets/new'
-  # get 'password_resets/edit'
-  # get 'users/new'
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  get 'users/new'
 
   root 'static_pages#home'
   get '/help',    to: 'static_pages#help'
